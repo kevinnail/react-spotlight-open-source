141b288ec5b6e1302686fec8c2123cad84611daf
kevinnail <fumalicious@gmail.com>
2023-01-31 10:44:13 -0800

    basic chart/ table components created, app.js refactored

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Charts from './components/Charts.js';
+import Table from './components/Table.js';
 
 function App() {
   return (
@@ -9,12 +11,8 @@ function App() {
         <Link to="/charts">Charts</Link>
       </nav>
       <Switch>
-        <Route path="/table">
-          <h1>Table Libary</h1>
-        </Route>
-        <Route path="/charts">
-          <h1>Chart Library</h1>
-        </Route>
+        <Route path="/table" component={Table}></Route>
+        <Route path="/charts" component={Charts}></Route>
       </Switch>
     </div>
   );
